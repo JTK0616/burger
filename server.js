975645d0f524f02b27f0097c502f280958dd1913
Jeff Knowles <knowles.jeffrey@gmail.com>
2017-05-03 22:56:55 -0400

    Homework update

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
+var exphbs  = require('express-handlebars');
+ 
+var app = express();
+ 
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+app.listen(3000);
 
